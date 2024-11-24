b1ea1744e6115abc8e9c899813de934453e249d3
Anastasiia Yerashova <yerashova.a@gmail.com>
2024-11-24 17:41:11 +0100

    update

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -30,7 +30,8 @@ export default function MoviesPage() {
     return (
         <div>  
             <SearchBar onSubmit={handleSearchSubmit} />
-            {newMovies.length === 0 && newQuery!=='' &&(<p className={s.text}>No movies found. Try searching for something else</p>)}
+            {newMovies.length === 0 && newQuery !== '' &&
+                (<p className={s.text}>No movies found. Try searching for something else</p>)}
             <MovieList movies={newMovies}/>
         </div>
     )
